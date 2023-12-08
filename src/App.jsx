7da17278f1d798c319d5e7ffe8a4b96d7a1347ce
Manya Jain <manyajain62002@gmail.com>
2023-12-08 17:14:05 +0530

    tailwind rror

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Container from "./Components/Container";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <h1>check</h1>;
+  return (
+    <div className="h-full border-2 border-black bg-black font-extrabold">
+      checl
+      {/* <Header />
+      <Container />
+      <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
+
+/*
+APP:
+  -LogIN
+
+  Header
+  - Container 
+  - Profile 
+  - Create Profile 
+  - Repo Open 
+  Footer
+*/
